38a721f18d34e86a812494b5c38c56b07dba21fe
Dmitrandir <dim9107@mail.ru>
2022-10-24 20:11:45 +0300

    Initial Commit

---
+++ HappyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyDay
+//
+//  Created by Дмитрий Солопов on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
