7df4137c05e5d690e04cf5d9421824e3450bee85
Dmitrandir <dim9107@mail.ru>
2022-10-26 23:02:11 +0300

    Add TableView and TaleViewCell

--- HappyDay/ContactsViewController.swift
+++ HappyDay/ContactsViewController.swift
@@ -11,9 +11,21 @@ class ContactsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
+extension  ContactsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell index: \(indexPath.row)"
+        return cell
+    }
+}
